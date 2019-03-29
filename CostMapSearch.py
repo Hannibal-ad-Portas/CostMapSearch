14a71e61aeb733bc78c03bf82922084515f0de6f
James Conroy <jconroy12@gmail.com>
2019-03-29 14:34:24 -0400

    Initial Commit

---
+++ CostMapSearch.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# This program generates a path using several search algorithims
+
+import sys, getopt
+
+def main (argv):
+    method = ''
+    infile = ''
+    rstart
+    cstart
+    rend
+    cend
+    outfile = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "", [])
+    except getopt.GetoptError:
+        print ('Error with inputs')
+        sys.exit(2)
+    for :w
+    hj
